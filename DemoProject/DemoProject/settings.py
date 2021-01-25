8eeff52269ce57356b46d8d6d766dacf2ded7844
Dagwood <sudhagarnarayanan@gmail.com>
2021-01-25 18:02:03 +0530

    host updated

--- DemoProject/DemoProject/settings.py
+++ DemoProject/DemoProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ngegl4ayje%@ddplbuv^j#8)2h-m=2v#1sh1j2bg2vf&*14j3f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ec2-52-66-125-118.ap-south-1.compute.amazonaws.com', '127.0.0.1']
 
 
 # Application definition
